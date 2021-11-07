86ee5b6e8fdf662acc22cccd9eeb1b26a102d66e
Stanislav Kukucka <89223349+stanislav-kukucka@users.noreply.github.com>
2021-11-07 16:53:00 +0100

    Add files via upload

---
+++ keywordology.py
@@ -0,0 +1,18 @@
+#this one works
+#pip3 install requests
+
+
+
+import requests
+import json
+lib1 = input("Enter Base Keyword to Suggest: ")
+lib2 = input("Enter Letter, Number or Word to Suggest: ")
+url = "http://suggestqueries.google.com/complete/search?client=firefox&q=" +(str(lib1)) +"%20" +(str(lib2))
+
+headers = {'USER_AGENT':'Mozilla/5.0'}
+response = requests.get(url, headers=headers)
+research = json.loads(response.content.decode('cp1252'))
+
+serializeddata = json.dumps(research, indent=4, sort_keys=True) # make json output pretty
+
+print(serializeddata)
